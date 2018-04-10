b2a6819f74d95c39f63b6e800f8b29f817b9003b
Rogoths <r.muny@hotmail.fr>
2018-04-10 09:57:29 +0200

    change query to execute

--- openfoodfacts_url.py
+++ openfoodfacts_url.py
@@ -12,32 +12,38 @@ def openfoodfacts_categories():
     url = requests.get("https://fr.openfoodfacts.org/langue/francais/categories.json")
     data_raw = url.json()
     data_categories = data_raw["tags"]
-    limit_categories = 0
-    for categories in data_categories:
-        if limit_categories < 10:
-            name = str(categories["name"])
-            insert_categories_db(name)
-            limit_categories += 1
+    for categories in data_categories[0:10]:
+        name_cat = str(categories["name"])
+        print(name_cat)
+        insert_categories_db(name_cat)
 
-def insert_categories_db(name):
+def insert_categories_db(name_cat):
 
-    con_db = db_connection()
-    con_db.query(
-        "INSERT INTO categories"
-        "VALUES (NULL, '{0}')".format(name)
+    curs, con_db = db_connection() #tuples
+    curs.execute(
+        "INSERT categories VALUES (NULL, %s)",name_cat
     )
-"""
-def openfoodfacts_produits(categorie):
+    con_db.commit()
 
-    url = requests.get("https://fr.openfoodfacts.org/langue/francais/categorie/"+str(categorie)".json")
+def openfoodfacts_produits():
+
+    url = requests.get("https://fr.openfoodfacts.org/langue/francais/categorie/boissons.json")
     data_raw = url.json()
     data_produits = data_raw["products"]
-    dict_produits = {}
     for produits in data_produits:
-        dict_produits.append(produits["product_name_fr"])
+        name_prod = str(produits["product_name_fr"])
+        id_prod = str(produits["_id"])
+        print(id_prod, name_prod)
+        insert_produits_db(id_prod, name_prod)
+
+def insert_produits_db(id_prod, name_prod):
+
+    curs, con_db = db_connection() #tuples
+    curs.execute(
+        "INSERT produits VALUES (%s, %s)", id_prod, name_prod
+    )
+    con_db.commit()
 
 
-    for id_produit in data_produits
-"""
 
-openfoodfacts_categories()
+openfoodfacts_produits()

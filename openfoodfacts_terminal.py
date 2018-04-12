0be9ed1f42cc6d3949150842b7f9f0563c115c48
Rogoths <r.muny@hotmail.fr>
2018-04-12 12:16:49 +0200

    add test for url

---
+++ openfoodfacts_terminal.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*
+main_screen = True
+def display_choice():
+    product_choice = "Trouver un aliment dans la base de données. Tappez 1"
+    print(product_choice)
+
+def select_choice():
+    user_answer = input(":")
+
+    return user_answer
+
+def main_screen():
+    answer = display_choice()
+    if answer == 1:
+
+    
--- openfoodfacts_url.py
+++ openfoodfacts_url.py
@@ -3,6 +3,7 @@
 #import json
 import requests
 from openfoodfacts_db import db_connection
+from openfoodfacts_objects import Product
 
 def openfoodfacts_categories():
     """
@@ -27,15 +28,37 @@ def insert_categories_db(name_cat):
 
 def openfoodfacts_produits():
 
-    url = requests.get("https://fr.openfoodfacts.org/langue/francais/categorie/boissons.json")
+    url = requests.get("https://fr.openfoodfacts.org/langue/francais/categorie/barres.json")
     data_raw = url.json()
     data_produits = data_raw["products"]
+    prod_dict = {}
+    #list_id = []
     for produits in data_produits:
-        name_prod = str(produits["product_name_fr"])
+        name_prod = str(produits["product_name"])
         id_prod = str(produits["_id"])
-        print(id_prod, name_prod)
-        insert_produits_db(id_prod, name_prod)
+        try:
+            prod_dict[id_prod] = name_prod #dict for products with id
+        except KeyError:
+            print("keyerror!")
+    for prod_id, prod in prod_dict.items():
+        #list_id.append(id)
+        print(prod_id, prod)
+    #insert_produits_db(id_prod, name_prod)
 
+def openfoodfacts_produit():
+
+    url = requests.get("https://fr.openfoodfacts.org/api/v0/product/20110499.json")
+    data_raw = url.json()
+    data_produit = data_raw["product"]
+    product = Product()
+    product.nom = data_produit["product_name"]
+    product.marque = data_produit["brands"]
+    product.grade = data_produit["nutrition_grades"]
+    #product.url = "https://fr.openfoodfacts.org/produit/"+str(product_id)+str(product.nom)
+    print(product.nom, product.marque, product.grade)
+
+
+"""
 def insert_produits_db(id_prod, name_prod):
 
     curs, con_db = db_connection() #tuples
@@ -43,7 +66,8 @@ def insert_produits_db(id_prod, name_prod):
         "INSERT produits VALUES (%s, %s)", id_prod, name_prod
     )
     con_db.commit()
+"""
 
 
 
-openfoodfacts_produits()
+openfoodfacts_produit()
